f920f5be9287994dbeffa59444938fc246424fd8
Gordeev Sergey <gor08serg@ya.ru>
2016-07-03 22:25:34 +0300

    Добпален блок 'Важные дороги без названия'

--- config/tests.php
+++ config/tests.php
@@ -17,7 +17,7 @@ return [
 		'sql' => 'SELECT s.latitude, s.longitude, s.id, s.roadtype, s.street_id, s.lock
 			,str.name as str_name, str.isempty as str_isempty, str.city_id as str_city_id, c.name as c_name, c.isempty as c_isempty FROM segments AS s
 			LEFT JOIN streets as str ON (s.street_id = str.id) LEFT JOIN cities as c ON (str.city_id = c.id)
-			WHERE ((roadtype=2 and coalesce(lock,0) < 2) or (roadtype=7 and coalesce(lock,0) < 3) or (roadtype in (3,4,6) and coalesce(lock,0) < 4)) and area_id = :area_id LIMIT 1000',
+			WHERE ((roadtype=2 and coalesce(lock,0) < 2) or (roadtype=7 and coalesce(lock,0) < 3) or (roadtype in (3,4,6) and coalesce(lock,0) < 4)) and area_id = :area_id LIMIT 2000',
 		'fields' => [
 			'Расположение сегмента' => 'c_link',
 			'Тип дороги' => 'c_road_type',
@@ -29,7 +29,7 @@ return [
 		'sql' => 'SELECT s.latitude, s.longitude, s.id, s.roadtype, s.street_id, s.fwdmaxspeed, s.revmaxspeed, s.fwddirection, s.revdirection
 			,str.name as str_name, str.isempty as str_isempty, str.city_id as str_city_id, c.name as c_name, c.isempty as c_isempty FROM segments AS s
 			LEFT JOIN streets as str ON (s.street_id = str.id) LEFT JOIN cities as c ON (str.city_id = c.id)
-			WHERE ((fwddirection and fwdmaxspeedunverified) or (revdirection and revmaxspeedunverified)) and area_id = :area_id LIMIT 1000',
+			WHERE ((fwddirection and fwdmaxspeedunverified) or (revdirection and revmaxspeedunverified)) and area_id = :area_id LIMIT 2000',
 		'fields' => [
 			'Расположение сегмента' => 'c_link',
 			'Тип дороги' => 'c_road_type',
@@ -46,7 +46,7 @@ return [
 		'sql' => 'SELECT s.latitude, s.longitude, s.id, s.roadtype, s.street_id, s.fwdmaxspeed, s.revmaxspeed, s.fwddirection, s.revdirection
 			,str.name as str_name, str.isempty as str_isempty, str.city_id as str_city_id, c.name as c_name, c.isempty as c_isempty FROM segments AS s
 			LEFT JOIN streets as str ON (s.street_id = str.id) LEFT JOIN cities as c ON (str.city_id = c.id)
-			WHERE (roadtype in (2,3,4,6,7) and ((fwddirection and fwdmaxspeed is null) or (revdirection and revmaxspeed is null))) and area_id = :area_id ORDER BY s.city_id LIMIT 1000',
+			WHERE (roadtype in (2,3,4,6,7) and ((fwddirection and fwdmaxspeed is null) or (revdirection and revmaxspeed is null))) and area_id = :area_id ORDER BY s.city_id LIMIT 2000',
 		'fields' => [
 			'Расположение сегмента' => 'c_link',
 			'Тип дороги' => 'c_road_type',
@@ -57,20 +57,34 @@ return [
 		'sql' => 'SELECT s.latitude, s.longitude, s.id, s.roadtype, s.street_id, s.level
 			,str.name as str_name, str.isempty as str_isempty, str.city_id as str_city_id, c.name as c_name, c.isempty as c_isempty FROM segments AS s
 			LEFT JOIN streets as str ON (s.street_id = str.id) LEFT JOIN cities as c ON (str.city_id = c.id)
-			WHERE (s.level < -3 or s.level > 3) and area_id = :area_id ORDER BY s.city_id LIMIT 1000',
+			WHERE (s.level < -3 or s.level > 3) and area_id = :area_id ORDER BY s.city_id LIMIT 2000',
 		'fields' => [
 			'Возвышение' => 'level',
 			'Расположение сегмента' => 'c_link',
 			'Тип дороги' => 'c_road_type',
 		],
 	],
-	'no_name' => [
+	'not_completed' => [
 		'title' => 'Неподтвержденные сегменты',
 		'sql' => 'SELECT s.latitude, s.longitude, s.id, s.roadtype, s.street_id, s.last_edit_on, u.username as u_username, u.rank as u_rank
 			,str.name as str_name, str.isempty as str_isempty, str.city_id as str_city_id, c.name as c_name, c.isempty as c_isempty FROM segments AS s
 			LEFT JOIN streets as str ON (s.street_id = str.id) LEFT JOIN cities as c ON (str.city_id = c.id)
 			LEFT JOIN users as u on(u.id = s.last_edit_by)
-			WHERE (street_id is null) and area_id = :area_id ORDER BY s.city_id LIMIT 1000',
+			WHERE (street_id is null) and area_id = :area_id ORDER BY s.city_id LIMIT 2000',
+		'fields' => [
+			'Расположение сегмента' => 'c_link',
+			'Тип дороги' => 'c_road_type',
+			'Последнее обновление' => 'last_edit_on',
+			'Редактор' => 'c_editor',
+		],
+	],
+	'no_name' => [
+		'title' => 'Важные дороги без названия',
+		'sql' => 'SELECT s.latitude, s.longitude, s.id, s.roadtype, s.street_id, s.last_edit_on, u.username as u_username, u.rank as u_rank
+			,str.name as str_name, str.isempty as str_isempty, str.city_id as str_city_id, c.name as c_name, c.isempty as c_isempty FROM segments AS s
+			LEFT JOIN streets as str ON (s.street_id = str.id) LEFT JOIN cities as c ON (str.city_id = c.id)
+			LEFT JOIN users as u on(u.id = s.last_edit_by)
+			WHERE (str.isempty = TRUE and s.alt_names = FALSE AND roadtype in (3,6,7) AND roundabout = FALSE) and area_id = :area_id ORDER BY s.city_id LIMIT 2000',
 		'fields' => [
 			'Расположение сегмента' => 'c_link',
 			'Тип дороги' => 'c_road_type',
