93cbc5001bbd5b3be957edb0a6b452aed4bae539
Gordeev Sergey <gor08serg@ya.ru>
2016-07-10 12:14:58 +0300

    Добавлена возможность обновления исходников через браузер

--- Waze/Router.php
+++ Waze/Router.php
@@ -2,8 +2,6 @@
 
 namespace Waze;
 
-//use Waze\Controllers;
-
 class Router
 {
 	public static function process()
@@ -18,8 +16,13 @@ class Router
 		if ($request == '/map/') {
 			return new Controllers\Map();
 		}
+		if ($request == Config::get('git')) {
+			system('cd ..; git pull');
+			header('HTTP/1.0 404 Not Found');
+			die;
+		}
 		if (preg_match('~/test_area/(\d+)/~', $request, $m)) {
-			if(count($m) == 2) {
+			if (count($m) == 2) {
 				return new Controllers\Test([
 					'area_id' => $m[1],
 					'flag' => 'area',
