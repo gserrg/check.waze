1d009ffe6c75e8b1404c70865e037de64c600211
Gordeev Sergey <gor08serg@ya.ru>
2016-12-28 14:24:57 +0300

    Добавлены ссылки на на боксы сканирования

--- Waze/Controllers/Summary.php
+++ Waze/Controllers/Summary.php
@@ -10,7 +10,7 @@ class Summary extends Controller
 {
 	public function process()
 	{
-		$areas = DB::getInstance()->as_array('SELECT areas_mapraid.name, areas_mapraid.id, updates.updated_at
+		$areas = DB::getInstance()->as_array('SELECT areas_mapraid.name, areas_mapraid.id, updates.updated_at, states_shapes.hasc_1
 			FROM areas_mapraid
 			LEFT JOIN states_shapes ON(areas_mapraid.id = states_shapes.id_1)
 			LEFT JOIN updates ON(states_shapes.hasc_1 = updates.object) ORDER BY areas_mapraid.name');
