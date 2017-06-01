f3f513caf6eaab46c8cbbd3d022c19fb6726872f
Гордеев Сергей <gordeev@techart.ru>
2017-06-01 18:19:09 +0300

    Инициализация сайта переведена на composer

--- www/index.php
+++ www/index.php
@@ -1,10 +1,6 @@
 <?php
 
-	function __autoload($class) {
-		$class = '../' . str_replace("\\", '/', $class) . '.php';
-		/** @noinspection PhpIncludeInspection */
-		require_once($class);
-	}
+	include('../vendor/autoload.php');
 
 	$controller = Waze\Router::process();
 	echo $controller->process();
